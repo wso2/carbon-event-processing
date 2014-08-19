cfc7978f4426ea7994cf4f67dd0f4ad562147de6
mohanvive <mohan@wso2.com>
2014-08-19 18:17:35 +0530

    Fixes to http Input adaptor - license and tenant specific fixes

--- components/adaptors/event-input-adaptor/org.wso2.carbon.event.input.adaptor.http/src/main/java/org/wso2/carbon/event/input/adaptor/http/HTTPEventAdaptorType.java
+++ components/adaptors/event-input-adaptor/org.wso2.carbon.event.input.adaptor.http/src/main/java/org/wso2/carbon/event/input/adaptor/http/HTTPEventAdaptorType.java
@@ -1,17 +1,19 @@
 /*
- * Copyright 2004,2005 The Apache Software Foundation.
+ * Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
  */
 
 package org.wso2.carbon.event.input.adaptor.http;
@@ -29,16 +31,26 @@ import org.wso2.carbon.event.input.adaptor.core.message.config.InputEventAdaptor
 import org.wso2.carbon.event.input.adaptor.http.internal.ds.HTTPEventAdaptorServiceDS;
 import org.wso2.carbon.event.input.adaptor.http.internal.util.HTTPEventAdaptorConstants;
 
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public final class HTTPEventAdaptorType extends AbstractInputEventAdaptor {
 
     private static final Log log = LogFactory.getLog(HTTPEventAdaptorType.class);
     private static HTTPEventAdaptorType httpEventAdaptor = new HTTPEventAdaptorType();
     private ResourceBundle resourceBundle;
-    public static ConcurrentHashMap<Integer, Map<String, ConcurrentHashMap<String, ArrayList<HTTPAdaptorListener>>>> inputEventAdaptorListenerMap =
-            new ConcurrentHashMap<Integer, Map<String, ConcurrentHashMap<String, ArrayList<HTTPAdaptorListener>>>>();
+    public static ConcurrentHashMap<Integer, ConcurrentHashMap<String, ConcurrentHashMap<String, ArrayList<HTTPAdaptorListener>>>> inputEventAdaptorListenerMap =
+            new ConcurrentHashMap<Integer, ConcurrentHashMap<String, ConcurrentHashMap<String, ArrayList<HTTPAdaptorListener>>>>();
 
     public static ExecutorService executorService = new ThreadPoolExecutor(HTTPEventAdaptorConstants.ADAPTER_MIN_THREAD_POOL_SIZE,
                                                                            HTTPEventAdaptorConstants.ADAPTER_MAX_THREAD_POOL_SIZE, HTTPEventAdaptorConstants.DEFAULT_KEEP_ALIVE_TIME, TimeUnit.SECONDS,
@@ -118,7 +130,7 @@ public final class HTTPEventAdaptorType extends AbstractInputEventAdaptor {
 
         String topic = inputEventAdaptorMessageConfiguration.getInputMessageProperties().get(HTTPEventAdaptorConstants.ADAPTOR_MESSAGE_TOPIC);
         int tenantId = PrivilegedCarbonContext.getThreadLocalCarbonContext().getTenantId();
-        Map<String, ConcurrentHashMap<String, ArrayList<HTTPAdaptorListener>>> tenantSpecificListenerMap = inputEventAdaptorListenerMap.get(tenantId);
+        ConcurrentHashMap<String, ConcurrentHashMap<String, ArrayList<HTTPAdaptorListener>>> tenantSpecificListenerMap = inputEventAdaptorListenerMap.get(tenantId);
         if (tenantSpecificListenerMap == null) {
             tenantSpecificListenerMap = new ConcurrentHashMap<String, ConcurrentHashMap<String, ArrayList<HTTPAdaptorListener>>>();
             inputEventAdaptorListenerMap.put(tenantId, tenantSpecificListenerMap);
