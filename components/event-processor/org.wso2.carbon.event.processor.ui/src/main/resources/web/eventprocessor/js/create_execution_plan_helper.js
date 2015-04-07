52cae76683e332d86dfe2cc2710864bc32031ae8
dilini <dilini@wso2.com>
2015-04-07 16:03:09 +0530

    Adding autocompletion to code mirror

--- components/event-processor/org.wso2.carbon.event.processor.ui/src/main/resources/web/eventprocessor/js/create_execution_plan_helper.js
+++ components/event-processor/org.wso2.carbon.event.processor.ui/src/main/resources/web/eventprocessor/js/create_execution_plan_helper.js
@@ -1,12 +1,12 @@
 /*
- *  Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -149,13 +149,12 @@ function addImportedStreamDefinition() {
             var currentExecutionPlan = window.queryEditor.getValue();
             var executionPlanHeader = "";
             var executionPlanBody = "";
-
             if(currentExecutionPlan != "") {
                 var linesArray = currentExecutionPlan.split(SIDDHI_LINE_BREAK_CHARACTER);
                 var bodyBeginIndex = -1;
                 for (var i = 0; i < linesArray.length ; i++){
                     var line = linesArray[i];
-                    if(line.match(REGEX_STARTING_WITH_PLAN) != null
+                    if(line.match(REGEX_LINE_STARTING_WITH_PLAN) != null
                         || line.match(REGEX_LINE_STARTING_WITH_SINGLE_LINE_COMMENT) != null
                         || line.match(REGEX_LINE_STARTING_WITH_MULTI_LINE_COMMENT) != null
                         || line == ""){
