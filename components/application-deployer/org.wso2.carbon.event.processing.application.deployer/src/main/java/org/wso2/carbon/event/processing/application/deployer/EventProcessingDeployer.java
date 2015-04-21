9d8a5b3bdc8ea42c23399af5fd98656415657ec0
suhothayan <suhothayan@gmail.com>
2015-04-21 10:48:51 +0530

    removing files that are moved to carbon-analytics-common

--- components/application-deployer/org.wso2.carbon.event.processing.application.deployer/src/main/java/org/wso2/carbon/event/processing/application/deployer/EventProcessingDeployer.java
+++
@@ -1,9 +0,0 @@
-package org.wso2.carbon.event.processing.application.deployer;
-
-import org.apache.axis2.deployment.Deployer;
-import org.apache.axis2.deployment.repository.util.DeploymentFileData;
-
-public interface EventProcessingDeployer extends Deployer {
-    public void processDeployment(DeploymentFileData deploymentFileData) throws Exception;
-    public void processUndeployment(String filePath) throws Exception;
-}
