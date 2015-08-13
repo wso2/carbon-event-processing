ae8d5da5fc99c28b9746fa090212ebb240edd14a
dilini <dilini@wso2.com>
2015-08-13 13:38:42 +0530

    removing unnecessary lock when creating new instance

--- components/event-processor/org.wso2.carbon.event.processor.core/src/main/java/org/wso2/carbon/event/processor/core/internal/storm/status/monitor/StormStatusHolderInitializer.java
+++ components/event-processor/org.wso2.carbon.event.processor.core/src/main/java/org/wso2/carbon/event/processor/core/internal/storm/status/monitor/StormStatusHolderInitializer.java
@@ -2,12 +2,10 @@ package org.wso2.carbon.event.processor.core.internal.storm.status.monitor;
 
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
-import org.wso2.carbon.context.PrivilegedCarbonContext;
 import org.wso2.carbon.event.processor.core.internal.ds.EventProcessorValueHolder;
 import org.wso2.carbon.event.processor.core.internal.storm.StormTopologyManager;
 import org.wso2.carbon.event.processor.core.util.DistributedModeConstants;
 import org.wso2.carbon.event.processor.core.util.ExecutionPlanStatusHolder;
-import org.wso2.carbon.event.processor.manager.core.config.DistributedConfiguration;
 
 /**
  * Utility to initialize the statusHolder.
@@ -15,10 +13,6 @@ import org.wso2.carbon.event.processor.manager.core.config.DistributedConfigurat
 public class StormStatusHolderInitializer {
 
     public static void initializeStatusHolder(String executionPlanName, int tenantId, int parallel) {
-        String tenantDomain = PrivilegedCarbonContext.getThreadLocalCarbonContext().getTenantDomain();
-        DistributedConfiguration stormDeploymentConfiguration = EventProcessorValueHolder.getStormDeploymentConfiguration();
-        int lockTimeout = stormDeploymentConfiguration.getLockTimeout();
-
         String stormTopologyName = StormTopologyManager.getTopologyName(executionPlanName, tenantId);
 
         HazelcastInstance hazelcastInstance = EventProcessorValueHolder.getHazelcastInstance();
