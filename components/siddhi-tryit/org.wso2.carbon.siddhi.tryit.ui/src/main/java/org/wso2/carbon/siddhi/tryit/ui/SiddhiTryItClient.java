dfa8d696962b3795fd7762998fc9d9d3350031e6
thiliA <thilinia@wso2.com>
2015-09-21 11:38:47 +0530

    Fixing null check for execution plan runtime,  code formatting and updating license header for siddhi try it ui component

--- components/siddhi-tryit/org.wso2.carbon.siddhi.tryit.ui/src/main/java/org/wso2/carbon/siddhi/tryit/ui/SiddhiTryItClient.java
+++ components/siddhi-tryit/org.wso2.carbon.siddhi.tryit.ui/src/main/java/org/wso2/carbon/siddhi/tryit/ui/SiddhiTryItClient.java
@@ -1,16 +1,19 @@
 /*
  * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy
- * of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.siddhi.tryit.ui;
@@ -51,7 +54,7 @@ public class SiddhiTryItClient {
 
     private static Log log = LogFactory.getLog(SiddhiTryItClient.class);
     private static Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-    private String errMsg = "";
+    private String errMsg;
 
     /**
      * Event stream will be processed according to the specified execution plan
@@ -191,7 +194,7 @@ public class SiddhiTryItClient {
             log.error(errMsg, e);
             throw new Exception(errMsg, e);
         } finally {
-            if (!executionPlanRuntime.equals(null)) {
+            if (executionPlanRuntime != null) {
                 executionPlanRuntime.shutdown();
             }
         }
